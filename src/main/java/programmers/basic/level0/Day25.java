12f9d1dcdcbd4a55b093df8e58fa94bda2288454
jmr <jmr10200@gmail.com>
2022-12-22 22:16:10 +0900

    코딩테스트 입문 day25

---
+++ src/main/java/programmers/basic/level0/Day25.java
@@ -0,0 +1,102 @@
+package programmers.basic.level0;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Day25 {
+
+    /**
+     * 문자열 밀기
+     * @param A
+     * @param B
+     * @return A 를 밀어서 B가 될 수 있다면 몇 번 밀어야 하는지 횟수, B가 될 수 없으면 -1
+     */
+    public int solution1(String A, String B) {
+        int answer = 0;
+        // A 와 B 가 같으면 0번
+        if (A.equals(B)) {
+            return answer;
+        }
+        String[] splitA = A.split("");
+        String temp = "";
+        for (int i = 0; i < splitA.length; i++) {
+            // A 의 맨 마지막 글자
+            temp = splitA[splitA.length - 1];
+            // A 를 뒤에서부터 오른쪽으로 하나씩 밀기
+            for (int j = splitA.length - 1; j >= 1; j--) {
+                splitA[j] = splitA[j - 1];
+            }
+            // A 의 맨 마지막 글자 맨 앞으로
+            splitA[0] = temp;
+            answer++;
+            String pushA = Arrays.stream(splitA).collect(Collectors.joining()); // import java.util.stream.*;
+            if (B.equals(pushA)) {
+                return answer;
+            }
+        }
+        answer = -1;
+        return answer;
+    }
+
+    /**
+     * 종이 자르기
+     * @param M
+     * @param N
+     * @return M x N 크기의 종이를 1 x 1 크기로 자르기위한 최소 가위질 수, 겹쳐자를 수 없다.
+     */
+    public int solution2(int M, int N) {
+        int answer = 0;
+        if (M == 1 && N == 1) {
+            return answer;
+        } else {
+            int cutM = M - 1; // M 자르기
+            int cutN = N - 1; // N 자르기
+            answer = cutM + cutN * M;
+        }
+        return answer;
+    }
+
+    /**
+     * 연속된 수의 합
+     * @param num
+     * @param total
+     * @return 연속된 수 num 개를 더한 값이 total 이 될 때, 정수 배열
+     */
+    public int[] solution3(int num, int total) {
+        int[] answer = new int[num];
+
+        return answer;
+    }
+
+    /**
+     * 다음에 올 숫자
+     * @param common 등차수열 혹은 등비수열 , 공비가 0 인 경우는 없음
+     * @return 마지막 원소 다음으로 올 숫자
+     */
+    public int solution4(int[] common) {
+        int answer = 0;
+        int[] difference = new int[common.length - 1]; // 공차
+        int[] ratio = new int[common.length - 1]; // 공비
+        for (int i = 1; i < common.length; i++) {
+            difference[i - 1] = common[i] - common[i - 1];
+            if(common[i] != 0 && common[i - 1] != 0) { // 원소가 0인 경우 등차수열
+                ratio[i - 1] = common[i] / common[i - 1];
+            }
+        }
+        if (isDifference(difference)) {
+            answer = common[common.length - 1] + difference[0];
+        } else {
+            answer = common[common.length - 1] * ratio[0];
+        }
+        return answer;
+    }
+
+    private boolean isDifference(int[] difference) {
+        long differCnt = Arrays.stream(difference).distinct().count();
+        boolean result = false;
+        if (differCnt == 1) {
+            result = true;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/programmers/basic/level0/Day25Test.java
@@ -0,0 +1,86 @@
+package programmers.basic.level0;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day25Test {
+
+    Day25 day25 = new Day25();
+
+    @Test
+    @DisplayName("문자열 밀기")
+    void solution1() {
+        String a = "hello";
+        String b = "ohell";
+        int result = 1;
+        assertEquals(day25.solution1(a, b), result);
+
+        a = "apple";
+        b = "elppa";
+        result = -1;
+        assertEquals(day25.solution1(a, b), result);
+
+        a = "alive";
+        b = "alive";
+        result = 0;
+        assertEquals(day25.solution1(a, b), result);
+    }
+
+    @Test
+    @DisplayName("종이 자르기")
+    void solution2() {
+        int m = 2;
+        int n = 2;
+        int result = 3;
+        assertEquals(day25.solution2(m, n), result);
+
+        m = 2;
+        n = 5;
+        result = 9;
+        assertEquals(day25.solution2(m, n), result);
+
+        m = 1;
+        n = 1;
+        result = 0;
+        assertEquals(day25.solution2(m, n), result);
+    }
+
+    @Test
+    @DisplayName("연속된 수의 합")
+    void solution3() {
+        int num = 3;
+        int total = 12;
+        int[] result = {3, 4, 5};
+        assertEquals(day25.solution3(num, total), result);
+
+        num = 5;
+        total = 15;
+        result = new int[]{1, 2, 3, 4, 5};
+        assertEquals(day25.solution3(num, total), result);
+
+        num = 4;
+        total = 14;
+        result = new int[]{2, 3, 4, 5};
+        assertEquals(day25.solution3(num, total), result);
+
+        num = 5;
+        total = 5;
+        result = new int[]{-1, 0, 1, 2, 3};
+        assertEquals(day25.solution3(num, total), result);
+    }
+
+    @Test
+    @DisplayName("다음에 올 숫자")
+    void solution4() {
+        int[] common = {1, 2, 3, 4};
+        int result = 5;
+        assertEquals(day25.solution4(common), result);
+
+        common = new int[]{2, 4, 8};
+        result = 16;
+        assertEquals(day25.solution4(common), result);
+    }
+}
