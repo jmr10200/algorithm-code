f7a2ba36305c07bdc36018703ca4c461cfc658bf
jmr <jmr10200@gmail.com>
2023-01-13 19:29:54 +0900

    코딩테스트 Level1 day14

--- src/main/java/programmers/basic/level1/Level1Day14.java
+++ src/main/java/programmers/basic/level1/Level1Day14.java
@@ -27,5 +27,47 @@ public class Level1Day14 {
         return answer;
     }
 
+    private static final int DELETE = -9;
+
+    /**
+     * 체육복
+     * 바로 앞, 뒤 번호의 학생에게서만 체육복을 빌려올 수 있다.
+     * @param n 전체 학생 수
+     * @param lost 체육복을 도난당한 학생들의 번호가 담긴 배열
+     * @param reserve 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 , 도난당할 수 있다.
+     * @return 체육수업을 들을 수 있는 학생의 최댓값
+     */
+    public int solution2(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        // 여분의 체육이 없을때 현재 가능한 인원 수
+        int possibleNum = n - lost.length;
+        // 여벌의 체육복을 가진 학생이 도난 당한 경우, 여벌 배열에서 삭제
+        int reservableNum = 0;
+        for (int i = 0; i < reserve.length; i++) {
+            for (int j = 0; j < lost.length; j++) {
+                if (reserve[i] == lost[j]) {
+                    reservableNum++;
+                    lost[j] = DELETE; // 없애준다.
+                    reserve[i] = DELETE; // 없애준다.
+                    break;
+                }
+            }
+        }
+        // 빌릴 수 있는 학생 수
+        int studentCnt = 0;
+        for (int l : lost) {
+            for (int i = 0 ; i < reserve.length; i++) {
+                if (l - 1 == reserve[i] || l + 1 == reserve[i]) {
+                    studentCnt++;
+                    reserve[i] = DELETE;
+                    break;
+                }
+            }
+        }
+        answer = possibleNum + reservableNum + studentCnt;
+        return answer;
+    }
 
 }
--- src/test/java/programmers/basic/level1/Level1Day14Test.java
+++ src/test/java/programmers/basic/level1/Level1Day14Test.java
@@ -22,4 +22,40 @@ class Level1Day14Test {
         assertArrayEquals(day14.solution1(s), result);
     }
 
+    @Test
+    @DisplayName("체육복")
+    void solution2() {
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        int result = 5;
+        assertEquals(day14.solution2(n, lost, reserve), result);
+
+        n = 5;
+        lost = new int[]{2, 4};
+        reserve = new int[]{3};
+        result = 4;
+        assertEquals(day14.solution2(n, lost, reserve), result);
+
+        n = 3;
+        lost = new int[]{3};
+        reserve = new int[]{1};
+        result = 2;
+        assertEquals(day14.solution2(n, lost, reserve), result);
+
+        n = 7;
+        lost = new int[]{2, 4, 7};
+        reserve= new int[]{1, 3, 5};
+        result = 6;
+        assertEquals(day14.solution2(n, lost, reserve), result);
+
+        // 여벌 체육복을 가진 학생이 도난당한경우
+        n = 4;
+        lost = new int[]{2, 3};
+        reserve = new int[]{3, 4};
+        result = 3;
+        assertEquals(day14.solution2(n, lost, reserve), result);
+
+    }
+
 }
