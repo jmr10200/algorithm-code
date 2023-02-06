be528812a9a951ddeca99180bedceb757c02d9f7
jmr <jmr10200@gmail.com>
2023-02-06 19:33:34 +0900

    코딩테스트 Level1 개인정보 수집 유효기간

--- src/test/java/programmers/basic/level1/Level1Day16Test.java
+++ src/test/java/programmers/basic/level1/Level1Day16Test.java
@@ -41,4 +41,21 @@ class Level1Day16Test {
         result = 3;
         assertEquals(day16.solution2(s), result);
     }
+
+    @Test
+    @DisplayName("개인정보 수집 유효기간")
+    void solution3() {
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+        int[] result = {1, 3};
+        assertArrayEquals(day16.solution3(today, terms, privacies), result);
+
+        today = "2020.01.01";
+        terms = new String[]{"Z 3", "D 5"};
+        privacies = new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+        result = new int[]{1, 4, 5};
+        assertArrayEquals(day16.solution3(today, terms, privacies), result);
+    }
+
 }
