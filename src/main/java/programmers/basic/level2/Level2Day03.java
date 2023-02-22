36eeb1e26ee18175fc44916e22eb980f694ef8f0
jmr <jmr10200@gmail.com>
2023-02-22 19:15:42 +0900

    코딩테스트 Level2 영어 끝말잇기

--- src/main/java/programmers/basic/level2/Level2Day03.java
+++ src/main/java/programmers/basic/level2/Level2Day03.java
@@ -29,5 +29,44 @@ public class Level2Day03 {
         return answer;
     }
 
+    /**
+     * 영어 끝말잇기
+     * @param n n 명의 사람
+     * @param words 사람들이 순서대로 말한 단어
+     * @return 가장 먼저 탈락하는 사람의 번호 , 그 사람이 자신의 몇 번째 차례에 탈락하는지
+     */
+    public int[] solution2(int n, String[] words) {
+        // 가장 먼저 탈락하는 자 번호, 몇번째 차례에 탈락하는지
+        int[] answer = new int[2];
+        int count = 0; // 반복 카운트
+        Map<String, Integer> wordsMap = new HashMap<>();
+        for (String word : words) {
+            wordsMap.put(word, 0);
+        }
+        // 첫번쨰 글자
+        char start = words[0].charAt(0);
+        // 마지막 글자
+        char end = words[0].charAt(words[0].length() - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (i % n == 0) {
+                count++;
+            }
+            start = words[i].charAt(0);
+            // 잘못된 끝밀잇기 or 2번째 이상 등장인 경우
+            if ((i != 0 && end != start) || wordsMap.get(words[i]) >= 1) {
+                answer[0] = (i % n) + 1; // 탈락자 번호
+                answer[1] = count; // 몇번째인지
+                break;
+            }
+            // 정상인경우 카운트
+            wordsMap.put(words[i], wordsMap.getOrDefault(words[i], 0) + 1);
+            // 마지막 글자
+            end = words[i].charAt(words[i].length() - 1);
+        }
+
+        return answer;
+    }
+
+
 
 }
--- src/test/java/programmers/basic/level2/Level2Day03Test.java
+++ src/test/java/programmers/basic/level2/Level2Day03Test.java
@@ -22,4 +22,23 @@ class Level2Day03Test {
         assertEquals(day03.solution1(s), result);
     }
 
+    @Test
+    @DisplayName("영어 끝말잇기")
+    void solution2() {
+        int n = 3;
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        int[] result = {3, 3};
+        assertArrayEquals(day03.solution2(n, words), result);
+
+        n = 5;
+        words = new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+        result = new int[]{0, 0};
+        assertArrayEquals(day03.solution2(n, words), result);
+
+        n = 2;
+        words = new String[]{"hello", "one", "even", "never", "now", "world", "draw"};
+        result = new int[]{1, 3};
+        assertArrayEquals(day03.solution2(n, words), result);
+    }
+
 }
