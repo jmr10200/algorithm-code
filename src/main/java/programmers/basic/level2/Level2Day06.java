18684bc4cc16bdf27cd5298fc0eeafab0f914200
jmr <jmr10200@gmail.com>
2023-03-09 19:28:47 +0900

    코딩테스트 level2 기능 개발

--- src/main/java/programmers/basic/level2/Level2Day06.java
+++ src/main/java/programmers/basic/level2/Level2Day06.java
@@ -81,6 +81,44 @@ public class Level2Day06 {
         return answer;
     }
 
+    /**
+     * 기능 개발
+     * @param progresses 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열
+     * @param speeds 각 작업의 개발 속도가 적힌 정수 배열
+     * @return 각 배포마다 몇 개의 기능이 배포되는지
+     * 배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다.
+     */
+    public int[] solution3(int[] progresses, int[] speeds) {
+        int[] answer = {}; // 배포 마다 몇 개의 기능이 배포되는지를 배열에 담아 리턴
+        // 배포일
+        List<Integer> dates = new ArrayList<>();
+        // 배열로 순서대로 넣어야 한다.
+        for (int i = 0; i < progresses.length; i++) {
+            int date = (100 - progresses[i]) / speeds[i]; // 배포에 걸리는 날짜
+            // 나누어 떨어지지 않으면, 하루 더 걸리므로 배포일 + 1
+            if ((100 - progresses[i]) % speeds[i] != 0) {
+                date = date + 1;
+            }
+            dates.add(date);
+        }
+        List<Integer> result = new ArrayList<>();
+        int count = 0;
+        int releaseDate = dates.get(0); // 배포일
+        for (Integer date : dates) {
+            if (releaseDate < date) { // 7,3,9 -> 2,1
+                result.add(count);
+                count = 0;
+                releaseDate = date;
+            }
+            count++;
+        }
+        // 마지막 카운트
+        if (count != 0) {
+            result.add(count);
+        }
+        answer = result.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
 
 
 }
--- src/test/java/programmers/basic/level2/Level2Day06Test.java
+++ src/test/java/programmers/basic/level2/Level2Day06Test.java
@@ -46,4 +46,50 @@ class Level2Day06Test {
         assertArrayEquals(day06.solution2(s), result);
     }
 
+    @Test
+    @DisplayName("기능 개발")
+    void solution3() {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        int[] result = {2, 1};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+
+        progresses = new int[]{95, 90, 99, 99, 80, 99};
+        speeds = new int[]{1, 1, 1, 1, 1, 1};
+        result = new int[]{1, 3, 2};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+        // 5, 10, 1, 1, 20, 1
+        // 1, 3 , 2
+
+        progresses = new int[]{95, 95, 90, 99, 99, 80, 99};
+        speeds = new int[]{1, 1, 1, 1, 1, 1, 1};
+        result = new int[]{2, 3, 2};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+        // 5, 5, 10, 1, 1, 20, 1
+        // 2, 3, 2
+
+        progresses = new int[]{1, 1, 1, 1}; // 51, 101
+        speeds = new int[]{100, 50, 99, 100};
+        result = new int[]{1, 3};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+        // 1, 2 , 1, 1
+        // 1일차에 1개
+        // 2일차에 3개
+
+        progresses = new int[]{54, 58, 53, 1};
+        speeds = new int[]{100, 50, 99, 100};
+        result = new int[]{4};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+        // 1, 1, 1, 1
+        // 100-54 = 46 % 100
+
+        // 놓쳤던 테스트 케이스 : 첫째날 5일을 가장 큰 기준으로 잡아야 한다.
+        progresses = new int[]{95, 97, 96, 99};
+        speeds = new int[]{1, 1, 1, 1};
+        result = new int[]{4};
+        assertArrayEquals(day06.solution3(progresses, speeds), result);
+        // 5, 3, 4, 1
+        // 100-54 = 46 % 100
+    }
+
 }
