b5ba00bca92e1c532c6785752c2b76d4d811f247
jmr <jmr10200@gmail.com>
2023-03-12 13:35:22 +0900

    코딩테스트 level2 전화번호 목록

--- src/main/java/programmers/basic/level2/Level2Day07.java
+++ src/main/java/programmers/basic/level2/Level2Day07.java
@@ -18,6 +18,11 @@ public class Level2Day07 {
         // 원소의 길이만큼 반복
         for (int i = 0; i < elements.length; i++) {
             result.add(elements[i]);
+            // 1이면? (0) (1) (2) (3) (4)  = 7 9 1 1 4
+            // 2이면? (0,1) (1,2) (2,3) (3,4), (4,0) = 16 10 2 5 11
+            // 3이면? (0,1,2) (1,2,3) (2,3,4) (3,4,0) (4,0,1) = 17 11 6 12 20
+            // 4이면? (0,1,2,3) (1,2,3,4) (2,3,4,0) (3,4,0,1) (4,0,1,2) = 18 15 13 21 21
+            // 5이면? (0,1,2,3,4) = 22
             for (int j = 0; j < elements.length; j++) {
                 int sum = 0;
                 for (int k = 0; k < i + 1; k++) { // 개수
@@ -31,6 +36,35 @@ public class Level2Day07 {
             }
         }
         answer = result.size();
+        return answer;
+    }
+
+    /**
+     * 전화번호 목록
+     * @param phone_book 전화번호부에 적힌 전화번호를 담은 배열
+     * @return 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false, 그렇지 않으면 true
+     */
+    public boolean solution2(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length; i++) {
+            String target = phone_book[i]; // 접두어
+            int targetLength = target.length();
+            for (int j = i + 1; j < phone_book.length; j++) {
+                // 접두어가 되려면, 길이가 더 작아야함
+                if (targetLength <= phone_book[j].length()) {
+                    if (target.equals(phone_book[j].substring(0, targetLength))) {
+                        // 잘라서 일치하면
+                        answer = false;
+                        return answer;
+                    } else {
+                        break; // 정렬 특징 효율성
+
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 
--- src/test/java/programmers/basic/level2/Level2Day07Test.java
+++ src/test/java/programmers/basic/level2/Level2Day07Test.java
@@ -18,4 +18,25 @@ class Level2Day07Test {
 
     }
 
+    @Test
+    @DisplayName("전화번호 목록")
+    void solution2() {
+        String[] phoneBook = {"119", "97674223", "1195524421"};
+        //false
+        assertFalse(day07.solution2(phoneBook));
+
+        phoneBook = new String[]{"119", "25951", "97674223", "2295524421", "1195524421", "289552442", "20095524421"};
+        //false
+        // 정렬하면 ["119", "1195524421", "20095524421", "2295524421", "25951", "289552442", "97674223"]
+        assertFalse(day07.solution2(phoneBook));
+
+        phoneBook = new String[]{"123", "456", "789"};
+        //true
+        assertTrue(day07.solution2(phoneBook));
+
+        phoneBook = new String[]{"12", "123", "1235", "567", "88"};
+        //false
+        assertFalse(day07.solution2(phoneBook));
+    }
+
 }
